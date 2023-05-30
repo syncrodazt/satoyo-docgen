f170d9614aef2f356162704d98dd679415b5cb71
Thayakorn Rakwetpakorn <t.rakwetpakorn@outlook.com>
2023-05-30 17:47:36 +0900

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/satoyo-docgen/",
   };
 
   if (command !== "serve") {
